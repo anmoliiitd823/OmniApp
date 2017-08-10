06441f0e000820da52ed010316bd25e2a1462b4d
samazon <letcsam@gmail.com>
2017-08-10 13:19:51 +0500

    OmniAuth for Facebook+Twitter+Gihub in Rails Project

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :twitter, 'AWUQp4SioEcBFyqpKOHwjktg4', '0CKRsJllkD2qmJe2JZI0qdnK358L7oazPoqm9Ssp26FiWf6EuS'
-	provider :facebook, '1761556020551258', '2b88a895cc05a742f8d851f1034de989'
-	provider :github, '389620df448a9b8cccff', '87e1854f136a7bd174800b571b09397c8769a169'	
+	provider :twitter, 'Consumer Key (API Key)', 'Consumer Secret (API Secret)'
+	provider :facebook, 'App ID', 'App Secret'
+	provider :github, 'Client ID', 'Client Secret
+'	
 end
